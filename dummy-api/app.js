ae04053013a6bcf511bef0fff3b0d0b0c3dc157b
Anwesh43 <anweshthecool0@gmail.com>
2017-06-29 23:35:11 +0530

    changing response type from response to json

--- dummy-api/app.js
+++ dummy-api/app.js
@@ -3,7 +3,7 @@ const server = express()
 server.get('/task/:time',(req,res)=>{
     const time = parseInt(req.params.time || "1")*1000
     setTimeout(()=>{
-        res.json({status:"ok"})
+        res.send("hello")
     },time)
 })
 server.listen(8000)
